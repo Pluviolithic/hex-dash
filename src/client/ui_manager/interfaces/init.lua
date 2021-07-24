5a512982b59961cf30548d3b57c3e3e635d29a00
IdiosyncraticSociopath <phlegethon5889@gmail.com>
2021-07-24 01:10:19 -0400

    resolved ui initialization bug

--- src/client/ui_manager/interfaces/init.lua
+++ src/client/ui_manager/interfaces/init.lua
@@ -1,7 +1,8 @@
+local roact = require(game:GetService("ReplicatedStorage").Common.Roact)
 local interfaces = {}
 
 for _, interface in ipairs(script:GetChildren()) do
-    interfaces[interface.Name] = require(interface)
+    interfaces[interface.Name] = roact.createElement(require(interface))
 end
 
 return interfaces
